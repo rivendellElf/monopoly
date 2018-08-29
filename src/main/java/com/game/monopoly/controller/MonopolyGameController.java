579dbbb5c306bba027a6afca2d535c3c3003bf23
gopikrishhnachokkalamani <gopikrishhnachokkalamani@10.88.47.14>
2018-08-29 11:51:52 -0400

    updated

--- src/main/java/com/game/monopoly/controller/MonopolyGameController.java
+++ src/main/java/com/game/monopoly/controller/MonopolyGameController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.game.monopoly.model.GameSetup;
 
+
 @RestController
 @RequestMapping("/api/game/monopoly")
 public class MonopolyGameController {
