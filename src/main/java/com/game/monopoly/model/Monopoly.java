833447ed8d3d799e60e1cfa41fa29ca2336015bb
gopikrishhnachokkalamani <gopikrishhnachokkalamani@10.88.47.14>
2018-08-29 11:10:12 -0400

    updated code

---
+++ src/main/java/com/game/monopoly/model/Monopoly.java
@@ -0,0 +1,14 @@
+package com.game.monopoly.model;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_EMPTY)
+public class Monopoly implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
