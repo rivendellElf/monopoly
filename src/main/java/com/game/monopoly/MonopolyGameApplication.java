537ca9ecfc6055300169d8c0195412e06f81783b
gopikrishhnachokkalamani <gopikrishhnachokkalamani@10.88.47.14>
2018-08-29 10:55:50 -0400

    spring boot dependencies

---
+++ src/main/java/com/game/monopoly/MonopolyGameApplication.java
@@ -0,0 +1,12 @@
+package com.game.monopoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonopolyGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonopolyGameApplication.class, args);
+	}
+}
